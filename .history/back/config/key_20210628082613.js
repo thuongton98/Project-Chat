ac18a942f571d12b27b918bd3fea83bfa1354c9f
thuong <thuonglovevietnam@gmail.com>
2021-06-28 08:35:24 +0700

    t

---
+++ .history/back/config/key_20210628082613.js
@@ -0,0 +1,8 @@
+//dbPassword = 'mongodb://localhost:27017/chat';
+
+dbPassword = 'mongodb+srv://thuongadmin:thuong123@cluster0.gt3dp.mongodb.net/chat?retryWrites=true&w=majority';
+
+
+module.exports = {
+    mongoURI: dbPassword
+};
